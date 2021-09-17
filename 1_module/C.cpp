9e1f1a4bfa138e87510fc46ca19352719382ac5d
HvarZ <hvarzahar@gmail.com>
2021-09-17 23:37:36 +0300

    remove class receiver and add new more efficient for memory realization

--- 1_module/C.cpp
+++ 1_module/C.cpp
@@ -1,10 +1,10 @@
 #include <fstream>
 #include <string>
-#include <vector>
 #include <algorithm>
 #include <utility>
 #include <memory>
 
+#define EMPTY_POSITION -1
 
 class Queue {
 private:
@@ -23,14 +23,14 @@ public:
     void pop();
 };
 
-Queue::Queue() : data_(nullptr), capacity_(0), front_(-1), tail_(-1) {}
+Queue::Queue() : data_(nullptr), capacity_(0), front_(EMPTY_POSITION), tail_(EMPTY_POSITION) {}
 
 Queue::~Queue() {
     delete [] data_;
 }
 
 Queue::Queue(const std::string &writingFileName, const size_t capacity)
-            : data_(new std::string[capacity]), capacity_(capacity), front_(-1), tail_(-1) {
+            : data_(new std::string[capacity]), capacity_(capacity), front_(EMPTY_POSITION), tail_(EMPTY_POSITION) {
     writingStream_.open(writingFileName, std::ios::app);
 }
 
@@ -39,7 +39,7 @@ void Queue::push(const std::string &value) {
         writingStream_ << "overflow" << std::endl;
         return;
     }
-    if (front_ == -1) {
+    if (front_ == EMPTY_POSITION) {
         front_++;
     }
     tail_ = (tail_ + 1) % capacity_;
@@ -47,7 +47,7 @@ void Queue::push(const std::string &value) {
 }
 
 void Queue::print() noexcept {
-    if (front_ == -1) {
+    if (front_ == EMPTY_POSITION) {
         writingStream_ << "empty" << std::endl;
         return;
     }
@@ -58,128 +58,106 @@ void Queue::print() noexcept {
 }
 
 void Queue::pop() {
-    if (front_ == -1) {
+    if (front_ == EMPTY_POSITION) {
         writingStream_ << "underflow" << std::endl;
         return;
     }
     writingStream_ << data_[front_] << std::endl;
 
     if (front_ == tail_) {
-        front_ = -1; tail_ = -1;
+        front_ = EMPTY_POSITION;
+        tail_ = EMPTY_POSITION;
     }
     else {
         front_ = (front_ + 1) % capacity_;
     }
 }
 
-class Receiver {
-private:
-    std::vector<std::string> inputLines_;
-    std::ifstream readingStream_;
-    std::ofstream writingStream_;
 
+auto IsZeroSpace(const std::string& string) noexcept -> bool {
+    return std::find(string.begin(), string.end(), ' ') == string.end();
+}
 
-private:
-    [[nodiscard]] auto FindSetSize() const noexcept -> size_t {
-        for (size_t i = 0; i < inputLines_.size(); ++i) {
-            if (inputLines_[i].find("set_size") != std::string::npos) {
-                return i;
-            }
-        }
-        return -1;
+auto IsOneSpace(const std::string& string) noexcept -> bool {
+    size_t numberSpace = 0;
+    for (const auto& symbol : string) {
+        if (symbol == ' ') numberSpace++;
+        if (numberSpace > 1) return false;
     }
+    return true;
+}
 
-    [[nodiscard]] auto IsZeroSpace(const std::string& string) const noexcept -> bool {
-        return std::find(string.begin(), string.end(), ' ') == string.end();
+auto GetCommand(const std::string& string) noexcept -> std::string {
+    return std::string(string.begin(), std::find(string.begin(), string.end(), ' '));
+}
+
+auto GetValue(const std::string& string) noexcept -> std::string {
+    return std::string(std::next(std::find(string.begin(), string.end(), ' ')),
+                       string.end());
+}
+
+auto IsValidCommand(const std::string& string) noexcept -> bool {
+    auto command = GetCommand(string);
+
+    if ((command == "set_size" || command == "push") && IsOneSpace(string)) {
+        return true;
     }
-
-    [[nodiscard]] auto IsOneSpace(const std::string& string) const noexcept -> bool {
-        size_t numberSpace = 0;
-        for (const auto& symbol : string) {
-            if (symbol == ' ') {
-                numberSpace++;
-            }
-            if (numberSpace > 1) {
-                return false;
-            }
-        }
+    if ((string == "print" || string == "pop") && IsZeroSpace(string)) {
         return true;
     }
 
-    [[nodiscard]] auto GetCommand(const std::string& string) const noexcept -> std::string {
-        return std::string(string.begin(), std::find(string.begin(), string.end(), ' '));
-    }
+    return false;
+}
 
-    [[nodiscard]] auto GetValue(const std::string& string) const noexcept -> std::string {
-        return std::string(std::next(std::find(string.begin(), string.end(), ' ')),
-                           string.end());
-    }
+auto IsContainsSetSize(const std::string& line) noexcept -> bool {
+    return line.find("set_size") != std::string::npos && IsValidCommand(line);
+}
 
+int main(int argc, char* argv[]) {
+    std::string buffer;
 
-    [[nodiscard]] auto IsValidCommand(const std::string& string) const noexcept -> bool {
-        auto command = GetCommand(string);
+    std::ifstream readingStream;
+    std::ofstream writingStream;
+    readingStream.open(argv[1]);
+    writingStream.open(argv[2], std::ios::app);
 
-        if ((command == "set_size" || command == "push") && IsOneSpace(string)) {
-            return true;
+    bool isSetSizeFounded = false;
+
+    Queue* ptrQueue = nullptr;
+
+    while (getline(readingStream, buffer, '\n')) {
+        if (buffer.empty()) continue;
+        else if (!IsContainsSetSize(buffer) && !isSetSizeFounded) {
+            writingStream << "error" << std::endl;
+            continue;
         }
-        if ((string == "print" || string == "pop") && IsZeroSpace(string)) {
-            return true;
+        else if (IsContainsSetSize(buffer) && !isSetSizeFounded) {
+            ptrQueue = new Queue(argv[2],std::stoi(GetValue(buffer)));
+            isSetSizeFounded = true;
         }
 
-        return false;
-    }
-
-public:
-    explicit Receiver(const std::string &readingFileName) {
-        std::string buffer;
-        readingStream_.open(readingFileName);
-        while(std::getline(readingStream_, buffer, '\n')) {
-            if (buffer.empty()) continue;
-            inputLines_.push_back(std::move(buffer));
-        }
-    }
-
-    void Output(const std::string& writingFileName) {
-        writingStream_.open(writingFileName, std::ios::app);
-        size_t numberStrSetSize = FindSetSize();
-        if (numberStrSetSize == -1) {
-            for (size_t i = 0; i < inputLines_.size(); i++) {
-                writingStream_ << "error" << std::endl;
+        else if (IsValidCommand(buffer) && isSetSizeFounded) {
+            std::string command = GetCommand(buffer);
+            if (command == "push") {
+                ptrQueue->push(GetValue(buffer));
             }
-            return;
+            else if (command == "pop") {
+                ptrQueue->pop();
+            }
+            else if (command == "print") {
+                ptrQueue->print();
+            }
+            else
+                writingStream << "error" << std::endl;
         }
 
         else {
-            for (size_t i = 0; i < numberStrSetSize; ++i) {
-                writingStream_ << "error" << std::endl;
-            }
+            writingStream << "error" << std::endl;
         }
-        if (IsValidCommand(inputLines_[numberStrSetSize])) {
 
-            Queue queue(writingFileName, std::stoi(GetValue(inputLines_[numberStrSetSize])));
-            for (size_t i = numberStrSetSize + 1; i < inputLines_.size(); ++i) {
-                if (GetCommand(inputLines_[i]) == "push" && IsValidCommand(inputLines_[i])) {
-                    queue.push(GetValue(inputLines_[i]));
-                }
-                else if (GetCommand(inputLines_[i]) == "print" && IsValidCommand(inputLines_[i])) {
-                    queue.print();
-                }
-                else if (GetCommand(inputLines_[i]) == "pop" && IsValidCommand(inputLines_[i])) {
-                    queue.pop();
-                }
-                else {
-                    writingStream_ << "error" << std::endl;
-                }
-            }
-
-        }
     }
-};
 
-
-int main(int argc, char* argv[]) {
-    Receiver receiver(argv[1]);
-    receiver.Output(argv[2]);
+    delete ptrQueue;
     return 0;
 }
 
