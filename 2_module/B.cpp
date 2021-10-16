5fe1fc1cc5504bcc75bad9c330877776aa943c44
HvarZ <hvarzahar@gmail.com>
2021-10-17 00:14:25 +0300

    add delete and print functions

--- 2_module/B.cpp
+++ 2_module/B.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <utility>
+#include <queue>
+#include <string>
 
 template <typename K, typename T>
 struct Node {
@@ -110,6 +112,10 @@ public:
     new_node->parent = current;                         \
     Splay(new_node);                                    \
     root_ = new_node;
+
+#define DELETE_NODE(direction)                          \
+    root_ = target->direction;                          \
+    root_->parent = nullptr;
 
 template <typename K, typename T>
 class SplayTree {
@@ -223,32 +229,71 @@ public:
 
     }
 
-    void SetNode(const Node<K, T>& node) noexcept;
-    void DeleteNode(const K& key) noexcept;
+    void SetNode(const Node<K, T>& node) {
+        if (IsEmpty()) {
+            throw std::runtime_error("error");
+        }
+        auto target = SearchNode(node.key);
+        target->value = node.value;
+        Splay(target);
+    }
 
-    [[nodiscard]] auto SearchNode(const K& key) noexcept -> Node<K, T>& {
+    void DeleteNode(const K& key) {
+        auto* target = SearchNode(key);
+        if (target == nullptr) {
+            throw std::runtime_error("error");
+        }
+
+        else if (target->left != nullptr && target->right != nullptr) {
+            auto leftMaxChildTarget = target->left;
+            while (leftMaxChildTarget->right != nullptr) {
+                leftMaxChildTarget = leftMaxChildTarget->right;
+            }
+            Splay(leftMaxChildTarget);
+            Splay(target);
+
+            root_ = target->left;
+            root_->right = target->right;
+            target->right->parent = root_;
+            root_->parent = nullptr;
+
+        } else if (target->left == nullptr && target->right != nullptr) {
+            DELETE_NODE(right)
+
+
+        } else if (target->left != nullptr && target->right == nullptr) {
+            DELETE_NODE(left)
+
+        } else {
+            root_ = nullptr;
+        }
+
+        delete target;
+    }
+
+    [[nodiscard]] auto SearchNode(const K& key) noexcept -> Node<K, T>* {
         if (root_->key == key) {
-            return *root_;
+            return root_;
         }
         auto* current = root_;
         INFINITY {
             if (key > current->key) {
                 if (current->right == nullptr) {
                     Splay(current);
-                    return *current;
+                    return nullptr;
                 } else {
                     current = current->right;
                 }
             } else if (key < current->key) {
                 if (current->left == nullptr) {
                     Splay(current);
-                    return *current;
+                    return nullptr;
                 } else {
                     current = current->left;
                 }
             } else {
                 Splay(current);
-                return *current;
+                return current;
             }
         }
     }
@@ -285,24 +330,83 @@ public:
         }
     }
 
+    [[nodiscard]] auto GetRoot() const noexcept -> Node<K, T>* {
+        return root_;
+    }
+
 private:
     Node<K, T>* root_;
 };
 
 
 template <typename K, typename T>
-void Print(const SplayTree<K, T>& tree) noexcept;
+void Print(const SplayTree<K, T>& tree) noexcept {
+    if (tree.GetRoot() == nullptr) {
+        std::cout << "_\n";
+        return;
+    }
+    std::cout << "[" << std::to_string(tree.GetRoot()->key) << " "
+                     << tree.GetRoot()->value << "]\n";
+    std::queue<decltype(tree.GetRoot())> q;
+
+    q.push(tree.GetRoot()->left);
+    q.push(tree.GetRoot()->right);
+
+    decltype(tree.GetRoot()) current;
+
+    size_t elementCounter = 0;
+    size_t numberElements = 2;
+    std::string line;
+
+    while (!q.empty()) {
+        current = q.front();
+        q.pop();
+        if (current != nullptr) {
+            line += "[" + std::to_string(current->key) + " " + current->value
+                        + " " + std::to_string(current->parent->key) + "]";
+            q.push(current->left);
+            q.push(current->right);
+        } else {
+            line += "_";
+            q.push(nullptr);
+            q.push(nullptr);
+        }
+        ++elementCounter;
+        if (elementCounter == numberElements) {
+
+            std::string null_line = "_";
+            for (size_t j = 1; j < numberElements; ++j)
+                null_line += " _";
+
+            if (line != null_line) {
+                std::cout << line << '\n';
+                line.clear();
+            } else {
+                break;
+            }
+            numberElements *= 2;
+            elementCounter = 0;
+        } else {
+            line += " ";
+        }
+    }
+}
 
 int main() {
     SplayTree<int64_t, std::string> tree;
-    tree.AddNode(Node<int64_t, std::string>(215, "14"));
+    tree.AddNode(Node<int64_t, std::string>(8, "10"));
+    tree.AddNode(Node<int64_t, std::string>(4, "14"));
     tree.AddNode(Node<int64_t, std::string>(7, "15"));
-    tree.AddNode(Node<int64_t, std::string>(4, "13"));
+    tree.SetNode(Node<int64_t, std::string>(8, "11"));
+    tree.AddNode(Node<int64_t, std::string>(3, "13"));
     tree.AddNode(Node<int64_t, std::string>(5, "16"));
-    tree.AddNode(Node<int64_t, std::string>(115, "16"));
 
-    std::cout << tree.GetMin().value << std::endl;
-    std::cout << tree.GetMax().value << std::endl;
+    auto tmp = tree.SearchNode(88);
+    tmp = tree.SearchNode(7);
+
+    tree.DeleteNode(5);
+
+    Print(tree);
 
     return EXIT_SUCCESS;
 }
