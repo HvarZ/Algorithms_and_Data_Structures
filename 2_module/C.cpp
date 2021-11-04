db5c1c55ee126f0a0211fc4cc29efb7b7d5ef1ff
HvarZ <hvarzahar@gmail.com>
2021-11-04 19:10:36 +0300

    add full encapsulation in print function

--- 2_module/C.cpp
+++ 2_module/C.cpp
@@ -16,13 +16,6 @@ public:                                                     // some useful alias
     using keyValue_t = std::pair<K, V>;
 
 private:                                                    // service functions
-    [[nodiscard]] auto GetIndexLeftChild(const K& key) const noexcept -> index_t {
-        return 2 * mapIndex_.at(key) + 1;
-    }
-    [[nodiscard]] auto GetIndexRightChild(const K& key) const noexcept -> index_t {
-        return 2 * mapIndex_.at(key) + 2;
-    }
-
     [[nodiscard]] auto GetIndexLastNode() const noexcept -> index_t {
         return GetSize() - 1;
     }
@@ -132,8 +125,8 @@ private:                                                    // service functions
         K maxKey = tree_[0].first;
         index_t index;
         for (size_t i = 1; i < GetSize(); ++i) {
-            if (GetIndexLeftChild(tree_[i].first) >= GetSize() &&
-                GetIndexRightChild(tree_[i].first) >= GetSize()) {
+            if (2 * i + 1 >= GetSize() &&
+                2 * i + 2 >= GetSize()) {
                 if (tree_[i].first > maxKey) {
                     maxKey = tree_[i].first;
                     index = i;
@@ -151,11 +144,6 @@ private:                                                    // service functions
         return tree_.empty();
     }
 
-    [[nodiscard]] auto GetTree() const noexcept -> const std::vector<keyValue_t>* {
-        return &tree_;
-    }
-
-
     auto Extract() -> keyValue_t {
         if (IsEmpty()) {
             throw std::runtime_error("error");
@@ -165,47 +153,50 @@ private:                                                    // service functions
         return tmp;
     }
 
+    template<typename Key, typename Value>
+    friend void Print(std::ostream& out, const MinBinaryHeap<Key, Value>& heap);
+
 private:
     std::unordered_map<K, index_t> mapIndex_;
     std::vector<keyValue_t> tree_;
 };
 
 template <typename K, typename V>
-void Print(const MinBinaryHeap<K, V>& heap) {
+void Print(std::ostream& out, const MinBinaryHeap<K, V>& heap) {
     if (heap.IsEmpty()) {
-        std::cout << "_" << std::endl;
+        out << "_" << std::endl;
         return;
     }
 
-    std::cout << "[" << std::get<0>(heap.GetTree()->operator[](0))
-              << " " << std::get<1>(heap.GetTree()->operator[](0)) << "]" << " " << std::endl;
+    out << "[" << std::get<0>(heap.tree_[0])
+              << " " << std::get<1>(heap.tree_[0]) << "]" << " " << std::endl;
 
     size_t levelSize = 2;
     size_t counterElementLevel = 0;
 
     for (size_t i = 1; i < heap.GetSize(); ++i) {
-        std::cout << "[" << std::get<0>(heap.GetTree()->operator[](i)) << " "
-                         << std::get<1>(heap.GetTree()->operator[](i)) << " "
-                         << std::get<0>(heap.GetTree()->operator[]((i - 1) / 2)) << "]" << " ";
+        out << "[" << std::get<0>(heap.tree_[i]) << " "
+                         << std::get<1>(heap.tree_[i]) << " "
+                         << std::get<0>(heap.tree_[(i - 1) / 2]) << "]" << " ";
 
         counterElementLevel++;
         if (i + 1 == heap.GetSize()) {
             for (size_t j = 0; j < levelSize - counterElementLevel; ++j) {
-                std::cout << "_" << " ";
+                out << "_" << " ";
             }
-            std::cout << std::endl;
+            out << std::endl;
             break;
         }
         if (counterElementLevel == levelSize) {
             levelSize <<= 1;
-            std::cout << std::endl;
+            out << std::endl;
             counterElementLevel = 0;
         }
     }
 }
 
 template <typename K, typename V>
-void Handler(MinBinaryHeap<K, V>& heap) noexcept {
+void Handler(std::ostream& out, MinBinaryHeap<K, V>& heap) noexcept {
     std::string command, value;
     int64_t key = 0;
 
@@ -218,60 +209,60 @@ void Handler(MinBinaryHeap<K, V>& heap) noexcept {
                 std::cin >> key >> value;
                 heap.Add(key, value);
             } catch (std::runtime_error& e) {
-                std::cout << "error" << std::endl;
+                out << "error" << std::endl;
             }
         } else if (command == "set") {
             try {
                 std::cin >> key >> value;
                 heap.Set(key, value);
             } catch (std::runtime_error& e) {
-                std::cout << "error" << std::endl;
+                out << "error" << std::endl;
             }
         } else if (command == "delete") {
             try {
                 std::cin >> key;
                 heap.Delete(key);
             } catch (std::runtime_error& e) {
-                std::cout << "error" << std::endl;
+                out << "error" << std::endl;
             }
         } else if (command == "search") {
             try {
                 std::cin >> key;
                 auto tuple = heap.Search(key);
-                std::cout << "1 " << std::get<0>(tuple) << " "
+                out << "1 " << std::get<0>(tuple) << " "
                                   << std::get<1>(tuple) << std::endl;
             } catch (std::runtime_error& e) {
-                std::cout << "0" << std::endl;
+                out << "0" << std::endl;
             }
         } else if (command == "min") {
             try {
                 auto tuple = heap.GetMin();
-                std::cout << std::get<0>(tuple) << " "
+                out << std::get<0>(tuple) << " "
                           << std::get<1>(tuple) << " "
                           << std::get<2>(tuple) << std::endl;
             } catch (std::runtime_error& e) {
-                std::cout << "error" << std::endl;
+                out << "error" << std::endl;
             }
         } else if (command == "max") {
             try {
                 auto tuple = heap.GetMax();
-                std::cout << std::get<0>(tuple) << " "
+                out << std::get<0>(tuple) << " "
                           << std::get<1>(tuple) << " "
                           << std::get<2>(tuple) << std::endl;
             } catch (std::runtime_error& e) {
-                std::cout << "error" << std::endl;
+                out << "error" << std::endl;
             }
         } else if (command == "extract") {
             try {
                 auto pairKeyValue = heap.Extract();
-                std::cout << pairKeyValue.first << " " << pairKeyValue.second << std::endl;
+                out << pairKeyValue.first << " " << pairKeyValue.second << std::endl;
             } catch (std::runtime_error& e) {
-                std::cout << "error" << std::endl;
+                out << "error" << std::endl;
             }
         } else if (command == "print") {
-            Print(heap);
+            Print(out, heap);
         } else {
-            std::cout << "error" << std::endl;
+            out << "error" << std::endl;
         }
     }
 }
@@ -279,7 +270,7 @@ void Handler(MinBinaryHeap<K, V>& heap) noexcept {
 
 int main() {
     MinBinaryHeap<int64_t, std::string> heap;
-    Handler<int64_t, std::string>(heap);
+    Handler(std::cout, heap);
 
     return EXIT_SUCCESS;
 }
