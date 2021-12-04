d090c64aa82b2d7cdb17b6f6f853e27271f18fa9
HvarZ <hvarzahar@gmail.com>
2021-12-05 02:02:43 +0300

    finish case 3.D

--- 3_module/D.cpp
+++ 3_module/D.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <ostream>
 #include <iostream>
+#include <stdexcept>
+#include <algorithm>
 
 #define MAX_CHUNK_SIZE 8
 
@@ -11,7 +13,6 @@ auto isPrime(const uint64_t number) -> bool {
       return false;
     }
   }
-
   return true;
 }
 
@@ -27,7 +28,7 @@ public:
   }
 
   [[nodiscard]] auto exist(const uint64_t index) const noexcept -> bool {
-    return (data_[index / MAX_CHUNK_SIZE] & (1 << index % MAX_CHUNK_SIZE)) == 1;
+    return (data_[index / MAX_CHUNK_SIZE] & (1 << index % MAX_CHUNK_SIZE)) != 0;
   }
 
   [[nodiscard]] auto size() const noexcept -> uint64_t {
@@ -51,7 +52,7 @@ public:
 
   explicit BloomFilter(const uint64_t size, const double probability)
       : bitset_(std::round(-(size * std::log2(probability)) / std::log(2))),
-        hashSize_(std::round(-log2(probability))) {
+        hashSize_(std::round(-std::log2(probability))) {
     primeNumbers_.reserve(size);
     fillPrimeNumbers();
   }
@@ -62,13 +63,21 @@ public:
     }
   }
 
-  [[nodiscard]] auto search(const uint64_t key) const noexcept-> bool {
+  [[nodiscard]] auto search(const uint64_t key) const noexcept -> bool {
     for (uint64_t i = 0; i < hashSize_; ++i) {
       if (!bitset_.exist(hash(i, key))) {
         return false;
       }
     }
     return true;
+  }
+
+  [[nodiscard]] auto getSize() const noexcept -> uint64_t {
+    return bitset_.size();
+  }
+
+  [[nodiscard]] auto getHashSize() const noexcept -> uint64_t {
+    return hashSize_;
   }
 
   friend void print(std::ostream&, const BloomFilter& filter) noexcept;
@@ -102,11 +111,16 @@ private:
 void print(std::ostream& out, const BloomFilter& filter) noexcept {
   uint8_t bufferChunk;
   uint8_t maxValueChunk;
-  auto numberChunks = std::ceil(static_cast<float>(filter.bitset_.size()) / 8);
+  const auto remainder = filter.bitset_.size() % MAX_CHUNK_SIZE;
+  const auto numberChunks = std::ceil(static_cast<float>(filter.bitset_.size()) / 8);
   for (uint64_t i = 0; i < numberChunks; ++i) {
     maxValueChunk = UINT8_MAX;
-    if (i == numberChunks - 1) {
-      maxValueChunk = std::pow(filter.bitset_.size() % UINT8_MAX, 2) - 1;
+    if (i == numberChunks - 1 && remainder != 0) {
+      if (remainder == 1) {
+        maxValueChunk = 1;
+      } else {
+        maxValueChunk = std::pow(2, remainder) - 1;
+      }
     }
     bufferChunk = filter.bitset_.getChunk(i);
     while (maxValueChunk > 0) {
@@ -122,56 +136,122 @@ void print(std::ostream& out, const BloomFilter& filter) noexcept {
   out << std::endl;
 }
 
-auto isIntegerNumber(const std::string& command) noexcept -> bool {
-  return std::all_of(command.begin(), command.end(),
-                     [](const char symbol) { return isdigit(symbol) > 0; });
-}
+namespace Parsing {
+  auto isIntegerNumber(const std::string &command) noexcept -> bool {
+    return std::all_of(command.begin(), command.end(),
+                       [](const char symbol) { return isdigit(symbol) > 0; });
+  }
 
-auto isFloatNumber(const std::string& command) noexcept -> bool {
-  bool isFindPoint = false;
-  for (const auto& symbol : command) {
-    if (!std::isdigit(symbol)) {
-      if (symbol != '.' || isFindPoint == true) {
-        return false;
-      } else if (symbol == '.') {
-        isFindPoint = true;
+  auto isFloatNumber(const std::string &command) noexcept -> bool {
+    bool isFindPoint = false;
+    for (const auto &symbol : command) {
+      if (!std::isdigit(symbol)) {
+        if (symbol != '.' || isFindPoint == true) {
+          return false;
+        } else if (symbol == '.') {
+          isFindPoint = true;
+        }
       }
+    }
+    return true;
+  }
+
+  auto getCommand(const std::string &line) -> std::string {
+    return std::string(line.begin(), std::find(line.begin(), line.end(), ' '));
+  }
+
+  auto getValue(const std::string &line) -> uint64_t {
+    return std::stoull(std::string(
+        std::next(std::find(line.begin(), line.end(), ' ')), line.end()));
+  }
+
+  auto getSetValid(const std::string &line) -> std::pair<uint64_t, double> {
+    if (std::count(line.begin(), line.end(), ' ') != 2) {
+      throw std::logic_error("error");
     } else {
-      return false;
+      auto iter1 = std::find(line.begin(), line.end(), ' ');
+      auto iter2 = std::find(std::next(iter1), line.end(), ' ');
+      std::string command(line.begin(), iter1);
+      std::string n(std::next(iter1), iter2);
+      std::string P(std::next(iter2), line.end());
+      if (!(command == "set" && isIntegerNumber(n) && isFloatNumber(P))) {
+        throw std::logic_error("error");
+      } else {
+        if (std::stoull(n) == 0 || std::stod(P) > 1 ||
+            std::round(-std::log2(std::stod(P))) == 0) {
+          throw std::logic_error("error");
+        }
+        return std::make_pair(std::stoull(n), std::stod(P));
+      }
     }
   }
-  return true;
-}
 
-auto isSetValid(const std::string& line) noexcept -> bool {
-  if (std::count(line.begin(), line.end(), ' ') != 2) {
-    return false;
-  } else {
-    auto iter1 = std::find(line.begin(), line.end(), ' ');
-    auto iter2 = std::find(std::next(iter1), line.end(), ' ');
-    std::string command(line.begin(), iter1);
-    std::string n(std::next(iter1), iter2);
-    std::string P(std::next(iter2), line.end());
-    if (!(command == "set" && isIntegerNumber(n) && isFloatNumber(P))) {
+  auto isValidCommand(const std::string &line) noexcept -> bool {
+    size_t spaceCounter = std::count(line.begin(), line.end(), ' ');
+    if (spaceCounter > 1) {
       return false;
+    } else {
+      auto spaceIter = std::find(line.begin(), line.end(), ' ');
+      std::string command(line.begin(), spaceIter);
+      if (command == "print") {
+        return true;
+      } else if (command == "search" || command == "add") {
+        if (isIntegerNumber(std::string(std::next(spaceIter), line.end()))) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
     }
   }
-  return true;
 }
-
 
 
 int main() {
   std::string command;
+  std::ios::sync_with_stdio(false);
+  BloomFilter* iterFilter = nullptr;
   while (std::getline(std::cin, command, '\n')) {
     if (command.empty()) {
       continue;
-    } else if (isSetValid(command)) {
-      break;
     } else {
-      std::cout << "error\n";
+      try {
+        auto [n, P] = Parsing::getSetValid(command);
+        iterFilter = new BloomFilter(n, P);
+        std::cout << iterFilter->getSize() << " " << iterFilter->getHashSize() << std::endl;
+        break;
+      } catch (...) {
+        std::cout << "error\n";
+      }
     }
   }
 
+  while (std::getline(std::cin, command, '\n')) {
+    if (command.empty()) {
+      continue;
+    } else {
+      if (iterFilter == nullptr) {
+        break;
+      }
+      else if (Parsing::isValidCommand(command)) {
+        auto commandId = Parsing::getCommand(command);
+        if (commandId == "print") {
+          print(std::cout, *iterFilter);
+        } else if (commandId == "add") {
+          iterFilter->add(Parsing::getValue(command));
+        } else if (commandId == "search") {
+          iterFilter->search(Parsing::getValue(command)) ? std::cout << 1 << std::endl : std::cout << 0 << std::endl;
+        }
+      } else {
+        std::cout << "error\n";
+      }
+    }
+  }
+
+  if (iterFilter != nullptr) {
+    delete iterFilter;
+  }
   return EXIT_SUCCESS;
 }
