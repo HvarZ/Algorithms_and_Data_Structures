203b123991e9ac004923ac9aebff0073a4ed8c96
HvarZ <hvarzahar@gmail.com>
2022-02-02 14:34:48 +0300

    delete useful labs

---
+++ cmake-build-debug/CMakeFiles/1.C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1.C"
+  "1.C.pdb"
+  "CMakeFiles/1.C.dir/1_module/C.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1.C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/3.A.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3.A"
+  "3.A.pdb"
+  "CMakeFiles/3.A.dir/3_module/A.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3.A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
